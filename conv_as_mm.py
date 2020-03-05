f81fd278eb1fa761d1ec1ce4934ee41de7f30a95
Ibrahim Salihu Yusuf <sibrahim1396@gmail.com>
2020-03-05 15:35:00 +0200

    Added support for non-square images

--- conv_as_mm.py
+++ conv_as_mm.py
@@ -3,7 +3,7 @@
 # @Email:  sibrahim1396@gmail.com
 # @Project: Audio Classifier
 # @Last modified by:   yusuf
-# @Last modified time: 2020-03-02T09:34:58+02:00
+# @Last modified time: 2020-03-05T15:34:43+02:00
 
 
 
@@ -15,59 +15,65 @@ def convolution1(image, kernel, stride=1, padding=0):
     channels = image.shape[0]
     no_kernels = kernel.shape[0]
     kernel_size = kernel.shape[1]
-    image_size = image.shape[1]
-    fmap_size = int((image_size-kernel_size+(2*padding))/stride)+1
-    new_image = np.zeros((channels*kernel_size*kernel_size, fmap_size*fmap_size))
+    image_height = image.shape[1]
+    image_width = image.shape[2]
+    fmap_h = int((image_height-kernel_size+(2*padding))/stride)+1
+    fmap_w = int((image_width-kernel_size+(2*padding))/stride)+1
+    new_image = np.zeros((channels*kernel_size*kernel_size, fmap_h*fmap_w))
     fmaps = []
     if padding>0:
-        temp = np.zeros((channels,image_size+2*padding,image_size+2*padding))
+        temp = np.zeros((channels,image_height+2*padding, image_width+2*padding))
         temp[:,padding:-padding,padding:-padding] = image
         image = temp
-        image_size = image.shape[1]
+        image_height = image.shape[1]
+        image_width = image.shape[2]
         del temp
     idx, ch = 0,0
     while ch<channels:
         row,idx = 0,0
-        while row+kernel_size <= image_size:
+        while row+kernel_size <= image_height:
             col=0
-            while col+kernel_size <= image_size:
+            while col+kernel_size <= image_width:
                 new_image[kernel_size*kernel_size*ch:kernel_size*kernel_size*ch+(kernel_size*kernel_size), idx] = (image[ch,row:row+kernel_size,col:col+kernel_size]).reshape(-1)
                 idx+=1
                 col+=stride
             row+=stride
         ch+=1
     for k in range(no_kernels):
-        fmap = (kernel[k].reshape(-1) @ new_image).reshape(fmap_size,fmap_size).astype(np.int)
+        fmap = (kernel[k].reshape(-1) @ new_image).reshape(fmap_h,fmap_w).astype(np.int)
         fmaps.append(fmap)
-    return fmaps
+    return np.array(fmaps)
 
 #An implementation of https://medium.com/activating-robotic-minds/up-sampling-with-transposed-convolution-9ae4f2df52d0
 def convolution2(image, kernel, stride=1, padding=0):
     channels = image.shape[0]
     kernel_size = kernel.shape[1]
     no_kernels = kernel.shape[0]
-    image_size = image.shape[1]
-    fmap_size = int((image_size-kernel_size+(2*padding))/stride)+1
+    image_height = image.shape[1]
+    image_width = image.shape[2]
+    fmap_h = int((image_height-kernel_size+(2*padding))/stride)+1
+    fmap_w = int((image_width-kernel_size+(2*padding))/stride)+1
     fmaps=[]
     if padding>0:
-        temp = np.zeros((channels,image_size+2*padding,image_size+2*padding))
+        temp = np.zeros((channels,image_height+2*padding,image_width+2*padding))
         temp[:,padding:-padding,padding:-padding] = image
         image = temp
-        image_size = image.shape[1]
+        image_height = image.shape[1]
+        image_width = image.shape[2]
         del temp
     for k in range(no_kernels):
         row, col = 0,0
-        conv_matrix = np.zeros((fmap_size*fmap_size, image_size*image_size*channels))
+        conv_matrix = np.zeros((fmap_h*fmap_w, image_height*image_width*channels))
         idx=0
-        while row+kernel_size <= image_size:
+        while row+kernel_size <= image_height:
             col=0
-            while col+kernel_size<=image_size:
+            while col+kernel_size<=image_width:
                 temp_kernel = np.zeros_like(image)
                 temp_kernel[:,row:row+kernel_size, col:col+kernel_size] = kernel[k]
                 conv_matrix[idx] = temp_kernel.reshape(-1)
                 col+=stride
                 idx+=1
             row+=stride
-        fmap = (np.array(conv_matrix) @ image.reshape(-1)).reshape(fmap_size,fmap_size).astype(np.int)
+        fmap = (np.array(conv_matrix) @ image.reshape(-1)).reshape(fmap_h,fmap_w).astype(np.int)
         fmaps.append(fmap)
     return np.array(fmaps)
